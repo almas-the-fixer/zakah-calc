50667ba0126e3d7b774c19ef385bd3114fe6033d
almas-the-fixer <professionalhacker74001@gmail.com>
2025-12-15 14:24:09 +0530

    Initial Project Setup

---
+++ backend/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Use(cors.New())
+
+	app.Get("/", func (c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{"message": "Zakah API is Running!...", "status": "success"})
+	})
+
+	log.Fatal(app.Listen(":8080"))
+}
